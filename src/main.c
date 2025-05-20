40c22085b811d3e9c732dc50aaf56968c6c9c673
Luiz Guilherme Bandeira <arkanael@gmail.com>
2025-05-20 15:45:04 -0300

    fazendo a bola quicar

--- src/main.c
+++ src/main.c
@@ -3,6 +3,19 @@
 
 Sprite* ball;
 
+int ball_pos_x = 100;
+int ball_pos_y = 100;
+int ball_vel_x = 1;
+int ball_vel_y = 1;
+int ball_width = 8;
+int ball_height = 8;
+
+/*CONSTANTES PARA FAZER A BOLA QUICAR*/
+const int LEFT_EDGE = 0;
+const int RIGHT_EDGE = 320;
+const int TOP_EDGE = 0;
+const int BOTTOM_EDGE = 224;
+
 int main()
 {
 
@@ -16,9 +29,31 @@ int main()
 	PAL_setPalette(PAL1, bgtile.palette->data,DMA);
 	VDP_fillTileMapRect(BG_B,TILE_ATTR_FULL(PAL1,0,FALSE,FALSE,1),0,0,40,30);
 
+
+
+	/*FUNCOES */
+
+	/*MOVER A BOLA*/
+	void moveBall()
+	{
+		if(ball_pos_x <= LEFT_EDGE || ball_pos_x >= RIGHT_EDGE - ball_width)
+		{
+			ball_vel_x = -ball_vel_x;
+		}else if(ball_pos_y <= TOP_EDGE || ball_pos_y >= BOTTOM_EDGE - ball_height)
+		{
+			ball_vel_y = -ball_vel_y;
+		}
+
+		ball_pos_x += ball_vel_x;
+		ball_pos_y += ball_vel_y;
+
+		SPR_setPosition(ball, ball_pos_x, ball_pos_y);
+	}
+
     while(1)
     {
 
+		moveBall();
 		SPR_update();
 		SYS_doVBlankProcess();
     }
